33b48e224b30d97f299a88cdb914831db07e4e67
dhouhasagga <saggadhouha@gmail.com>
2023-04-10 19:25:12 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+ const [person, setperson] = useState({
+  FullName:"Dhouha Sagga",
+  Bio:"Student",
+  imgSrc:"me.jpg",
+  Profession:"Fullstack Js",
+ });
+ const [show, setshow] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+     { show?(<div>
+      <h1>{person.FullName}</h1>
+      <img src={person.imgSrc}/>
+      <h2>{person.Bio}</h2>
+      <input type='text' onChange={(e)=> setperson({...person, Bio:e.target.value})}/>
+      <h2>{person.Profession}</h2>
+      <input type='text' onChange={(e)=> setperson({...person, Profession:e.target.value})}/>
+      <br></br>
+      </div>):null}
+      <button onClick={()=>setshow(!show)}> Click Here to show student</button>
     </div>
   );
 }
